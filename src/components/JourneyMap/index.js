95e7d0e95bbce384457a1731c3a54e1b46c9c819
Francis Marineau <frank.marineau@gmail.com>
2017-03-11 18:27:50 -0500

    meow

--- src/components/JourneyMap/index.js
+++ src/components/JourneyMap/index.js
@@ -1,40 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withGoogleMap, GoogleMap, Marker, Polyline } from 'react-google-maps'
 
-const onMapLoad = steps => map => {
-  const stepCoords = steps.map(step =>
-    new google.maps.LatLng(step.station.pos[0], step.station.pos[1])
-  )
-  const bounds = new google.maps.LatLngBounds()
+class JourneyMap extends Component {
+  onMapLoad = map => {
+    if (!map) return
+    const stepCoords = this.props.journey.steps.map(step =>
+      new google.maps.LatLng(step.station.pos[0], step.station.pos[1])
+    )
+    const bounds = new google.maps.LatLngBounds()
 
-  stepCoords.forEach(coord => {
-    bounds.extend(coord)
-  })
+    stepCoords.forEach(coord => {
+      bounds.extend(coord)
+    })
 
-  map.fitBounds(bounds)
+    map.fitBounds(bounds)
+  }
+
+  render() {
+    const { steps } = this.props.journey
+
+    return (
+      <GoogleMap
+        ref={this.onMapLoad}
+        defaultZoom={13}
+        defaultCenter={{ lat: steps[0].station.pos[0], lng: steps[0].station.pos[1] }}
+        scrollwheel={false}
+        draggable={false}
+        navigationControl={false}
+        mapTypeControl={false}
+        scaleControl={false}
+      >
+        {steps.map((step, i) => (
+          <Marker
+            key={step.id}
+            position={{ lat: step.station.pos[0], lng: step.station.pos[1] }}
+            label={String(i + 1)}
+          />
+        ))}
+
+        <Polyline path={steps.map(s => ({ lat: s.station.pos[0], lng: s.station.pos[1] }))} />
+      </GoogleMap>
+    )
+  }
 }
-
-const JourneyMap = ({ journey: { steps } }) => (
-  <GoogleMap
-    ref={onMapLoad(steps)}
-    defaultZoom={13}
-    defaultCenter={{ lat: steps[0].station.pos[0], lng: steps[0].station.pos[1] }}
-    scrollwheel={false}
-    draggable={false}
-    navigationControl={false}
-    mapTypeControl={false}
-    scaleControl={false}
-  >
-    {steps.map((step, i) => (
-      <Marker
-        key={step.id}
-        position={{ lat: step.station.pos[0], lng: step.station.pos[1] }}
-        label={String(i + 1)}
-      />
-    ))}
-
-    <Polyline path={steps.map(s => ({ lat: s.station.pos[0], lng: s.station.pos[1] }))} />
-  </GoogleMap>
-)
-
 export default withGoogleMap(JourneyMap)
